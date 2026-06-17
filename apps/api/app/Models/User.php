e5abc137c15295b67e767560c17933af05c83489
Osman Orhan <osman.orhan@gmail.com>
2026-06-17 16:52:24 +0300

    ci: fix lint errors

--- apps/api/app/Models/User.php
+++ apps/api/app/Models/User.php
@@ -9,8 +9,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(["name", "email", "password"])]
-#[Hidden(["password", "remember_token"])]
+#[Fillable(['name', 'email', 'password'])]
+#[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
@@ -24,8 +24,8 @@ class User extends Authenticatable
     protected function casts(): array
     {
         return [
-            "email_verified_at" => "datetime",
-            "password" => "hashed",
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
         ];
     }
 }
